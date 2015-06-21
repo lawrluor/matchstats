ca8e36a1eb7fc4bd72cf50e55ace3c496a209d59
Lawrence Luo <luolawrence1@gmail.com>
2015-06-21 14:14:00 -0700

    added config because we're basic

---
+++ config.py
@@ -0,0 +1,8 @@
+CSRF_ENABLED = True #Enables Cross-site Request Forgery. SECRET_KEY is needed when CSRF is enabled, and creates a cryptographic token used to validate a form. 
+SECRET_KEY = 'you-will-never-guess'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') #Uniform Resource Identifier, gives path for database file
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository') #folder where SQLAlchemy-migrate data files will be stored
