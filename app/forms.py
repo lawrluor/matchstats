3744f06f6628a90d963b03ac63ac838df5466eee
Lawrence Luo <luolawrence1@gmail.com>
2015-06-21 15:40:51 -0700

    winner character and loser character are now selected as a dropdown SelectField list rather than a string input

--- app/forms.py
+++ app/forms.py
@@ -15,10 +15,11 @@ class SetCreate(Form):
   set_max_match_count = SelectField('Best of:', choices = [('1','1'), ('3','3'), ('5','5'), ('7','7')], validators=[Required()]) #choices are strings, not integers, but are converted to integers in views.py - this is because it was buggy otherwise.
 
 class MatchSubmit(Form):
+  char_list = [('Fox', 'Fox'), ('Falco', 'Falco'), ('Sheik', 'Sheik'), ('Marth', 'Marth'), ('Jigglypuff', 'Jigglypuff'), ('Peach', 'Peach'), ('Captain Falcon', 'Captain Falcon'), ('Ice Climbers', 'Ice Climbers')] #Choices list for winner_char and loser_char SelectField; a constant value containing all the characters and not an input
   match_stage = SelectField('match_stage', choices = [('Battlefield', 'Battlefield'), ('Dream Land', 'Dream Land'), ('Final Destination', 'Final Destination'), ('Fountain of Dreams', 'Fountain of Dreams'), ('Pokemon Stadium', 'Pokemon Stadium'), ('Yoshi\'s Story', 'Yoshi\'s Story'), ('Other', 'Other')], coerce=str, validators=[Required()]) #optional input, so doesn't check if left blank
   match_winner = StringField('match_winner')
   match_loser = StringField('match_loser')
-  winner_char = StringField('winner_char')
-  loser_char = StringField('loser_char')
+  winner_char = SelectField('winner_char', choices=char_list) #Data no required in case no match info is known
+  loser_char = SelectField('loser_char', choices=char_list)
 
 #SelectField format for choices: The first (value, label) is the actual value. The label is what appears in the dropdown menu. In this case, both should be the same
