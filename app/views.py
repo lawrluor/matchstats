f077123330c902cdbde1d7ef353e7a34372f06d0
Lawrence Luo <luolawrence1@gmail.com>
2015-06-21 22:47:27 -0700

    implemented head to head page (route /versus) between two users, which currently displays their played sets

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
-#Currently returns index template to be displayed on client's web browser. Routes map URLs to the function.
+# Currently returns index template to be displayed on client's web browser. Routes map URLs to the function.
 
 from flask import render_template, flash, redirect, request, url_for
 from app import app, db
 from models import User, Set, Match
 from forms import UserCreate, SetCreate, MatchSubmit
+from sqlalchemy import and_
 
 @app.route('/')
 @app.route('/index')
@@ -12,7 +13,7 @@ def index():
                         title ='Home')
 
 
-@app.route('/browse_sets') #browse sets
+@app.route('/browse_sets') # browse sets
 def browse_sets():
   setlist = Set.query.all()
   return render_template("browse_sets.html",
@@ -20,7 +21,7 @@ def browse_sets():
                          setlist=setlist)
 
 
-@app.route('/browse_users') #browse users
+@app.route('/browse_users') # browse users
 def browse_users():
   userlist = User.query.all()
   return render_template("browse_users.html",
@@ -28,36 +29,36 @@ def browse_users():
                         userlist=userlist)
 
 
-@app.route('/user_create', methods=['GET', 'POST']) #'POST' allows us to receive POST requests, which will bring in form data entered by the user
+@app.route('/user_create', methods=['GET', 'POST']) # 'POST' allows us to receive POST requests, which will bring in form data entered by the user
 def user_create():
-  form = UserCreate() #instantiate object from UserCreate() class in app/forms.py
-  if form.validate_on_submit(): #if True, indicates data is valid and can be processed
-    created_tag = form.user_tag.data #stores entered value in variable
+  form = UserCreate() # instantiate object from UserCreate() class in app/forms.py
+  if form.validate_on_submit(): # if True, indicates data is valid and can be processed
+    created_tag = form.user_tag.data # stores entered value in variable
     created_region = form.user_region.data
     created_main = form.user_main.data
 
     
-    #create user row, initializing user object
+    # create user row, initializing user object
     new_user = User(tag=created_tag,
                 main=created_main,
                 region=created_region)
 
-    #commit to db
+    # commit to db
     db.session.add(new_user)
     db.session.commit()
     flash('User creation successful.')
 
     return redirect('/index')
-  return render_template('user_create.html', #renders template for creating user if called before user enters data
+  return render_template('user_create.html', # renders template for creating user if called before user enters data
                         title='Create User',
                         form=form)
 
 
-@app.route('/set_create', methods=['GET', 'POST']) #'POST' allows us to receive POST requests, which will bring in form data entered by the user
+@app.route('/set_create', methods=['GET', 'POST']) # 'POST' allows us to receive POST requests, which will bring in form data entered by the user
 def set_create():
-  form = SetCreate() #instantiate object from SetCreate() class in app/forms.py
-  if form.validate_on_submit(): #if True, indicates data is valid and can be processed
-    #Based on winner and loser tag submitted through form, queries User database to locate the respective Users
+  form = SetCreate() # instantiate object from SetCreate() class in app/forms.py
+  if form.validate_on_submit(): # if True, indicates data is valid and can be processed
+    # Based on winner and loser tag submitted through form, queries User database to locate the respective Users
     set_winner = User.query.filter(User.tag==form.set_winner_tag.data).first() 
     set_loser = User.query.filter(User.tag==form.set_loser_tag.data).first()
     
@@ -71,7 +72,7 @@ def set_create():
     created_total_matches = created_set_loser_score + created_set_winner_score
     created_max_match_count = int(form.set_max_match_count.data)
    
-    #Check to see if set score count is valid for type of set, and winner score>loser score
+    # Check to see if set score count is valid for type of set, and winner score>loser score
     if ((created_set_winner_score <= created_set_loser_score) or 
       ((created_set_winner_score > ((created_max_match_count / 2.0) + 1)) or 
       (created_set_winner_score < (created_max_match_count / 2.0)))):
@@ -79,7 +80,7 @@ def set_create():
       flash("Check to make sure you have entered the appropriate scores for the set score cout.")
       return redirect(url_for('set_create'))
 
-    #create set row, initializing set object
+    # create set row, initializing set object
     new_set = Set(
                   winner_tag=set_winner_tag,
                   loser_tag=set_loser_tag,
@@ -91,28 +92,28 @@ def set_create():
                   total_matches=created_total_matches
                   )
     
-    #Add winner_id and loser_id attributes for Set; must be done after creating the Set object so that it can call the getSetWinnerID and getSetLoserID functions
+    # Add winner_id and loser_id attributes for Set; must be done after creating the Set object so that it can call the getSetWinnerID and getSetLoserID functions
     new_set.winner_id = new_set.getSetWinnerID()
     new_set.loser_id = new_set.getSetLoserID()
 
-    #commit to db
+    # commit to db
     db.session.add(new_set)
     db.session.commit()
-    flash('Next, enter data for the individual matches.') #if Set is created successfully, redirect to the match_create page, where data for individual matches entered
+    flash('Next, enter data for the individual matches.') # if Set is created successfully, redirect to the match_create page, where data for individual matches entered
 
     return redirect(url_for('match_submit', set_id=str(new_set.id), total_matches=int(new_set.total_matches)))
-  return render_template('set_create.html', #renders template for creating user if called before user enters data
+  return render_template('set_create.html', # renders template for creating user if called before user enters data
                         title='Create Set', 
                         form=form
                         )
 
 @app.route('/match_submit', methods=['GET', 'POST'])
-def match_submit(): #set_id, total_matches passed through url from route /set_create as query strings automatically because they weren't passed as parameters of match_sbumit or part of the route /match_submit
+def match_submit(): # set_id, total_matches passed through url from route /set_create as query strings automatically because they weren't passed as parameters of match_sbumit or part of the route /match_submit
  
   set_id = request.args.get('set_id')
   total_matches = request.args.get('total_matches')
   
-  form = MatchSubmit() #instantiate object from MatchSubmit() class in app/forms.py
+  form = MatchSubmit() # instantiate object from MatchSubmit() class in app/forms.py
   if form.validate_on_submit():
     submitted_match_stage = form.match_stage.data
     submitted_match_winner = form.match_winner.data
@@ -129,17 +130,17 @@ def match_submit(): #set_id, total_matches passed through url from route /set_cr
                       set_id=set_id 
                       )
     
-    #commit to db
+    # commit to db
     db.session.add(new_match)
     db.session.commit()
     
-    #code for submitting the next match in the set
-    total_matches = int(total_matches) - 1 #now that one match has been submitted, there is one less match to be submitted out of the total number initially indicated when set was created
-    if total_matches == 0: #base case: if no more matches to submit, redirected home
+    # code for submitting the next match in the set
+    total_matches = int(total_matches) - 1 # now that one match has been submitted, there is one less match to be submitted out of the total number initially indicated when set was created
+    if total_matches == 0: # base case: if no more matches to submit, redirected home
       flash('Submission Complete') 
       return redirect('/index')
     else:
-      total_matches = str(total_matches) #necessary to convert again for flash message and more importantly as a parameter of function match_submit
+      total_matches = str(total_matches) # necessary to convert again for flash message and more importantly as a parameter of function match_submit
       flash('Match Submission Complete. Submit next Match. Matches left: ' + total_matches)
       return redirect(url_for('match_submit', set_id=set_id, total_matches=total_matches))
 
@@ -148,13 +149,13 @@ def match_submit(): #set_id, total_matches passed through url from route /set_cr
                           form=form)
 
 
-@app.route('/user/<tag>') #User profile page
+@app.route('/user/<tag>') # User profile page
 def user(tag):
-  user = User.query.filter(User.tag==tag).first() #If routed to user profile page (user/<tag>), check to make sure user exists
+  user = User.query.filter(User.tag==tag).first() # If routed to user profile page (user/<tag>), check to make sure user exists
   if user is None:
     flash('User %s not found.' % tag)
     return redirect(url_for('index'))
-  user_sets = user.getAllSets() #Store all user's sets in variable user_sets
+  user_sets = user.getAllSets() # Store all user's sets in variable user_sets
   user_lost_sets = user.getLostSets()
   user_won_sets = user.getWonSets()
   return render_template("user.html",
@@ -162,18 +163,32 @@ def user(tag):
                         user=user,
                         user_sets=user_sets,
                         user_lost_sets=user_lost_sets,
-                        user_won_sets=user_won_sets) #pass user's sets in variable user_sets  to form user.html 
+                        user_won_sets=user_won_sets) # pass user's sets in variable user_sets  to form user.html 
 
-@app.route('/region/<region>') #View all users in certain region
+
+#  User head to head page
+@app.route('/versus/<tag1>/<tag2>')
+def versus(tag1, tag2):
+  # Get all sets that user1 and user2 have played
+  sets = Set.query.filter(and_(Set.winner_tag==tag1, Set.loser_tag==tag2)).all()
+  sets = sets + Set.query.filter(and_(Set.winner_tag==tag2, Set.loser_tag==tag1)).all()
+  
+  return render_template("versus.html",
+                         tag1=tag1,
+                         tag2=tag2,
+                         setlist=sets)
+
+
+@app.route('/region/<region>') # View all users in certain region
 def view_region(region):
-  users = User.query.filter(User.region==region).all() #checks to see if user.region is identical to region
+  users = User.query.filter(User.region==region).all() # checks to see if user.region is identical to region
   if user is None:
-    flash('No players found in this region') #no user found with matching region
+    flash('No players found in this region') # no user found with matching region
   return render_template("view_region.html",
                          users=users,
                          region=region)
 
-#During production mode (runp.py), debug is turned OFF and these error templates appear
+# During production mode (runp.py), debug is turned OFF and these error templates appear
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
