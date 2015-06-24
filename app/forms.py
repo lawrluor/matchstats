f1621962981ad3653407ac6cf1e5493e77d1dfbf
Lawrence Luo <luolawrence1@gmail.com>
2015-06-24 10:56:24 -0700

    /user_create now requests for character main using a dropdown (SelectField) list

--- app/forms.py
+++ app/forms.py
@@ -2,10 +2,14 @@ from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField, SelectField, IntegerField # open fields to take input from person
 from wtforms.validators import DataRequired, InputRequired, Required # checks to make sure field isn't empty
 
+# character choice list for SelectField; a constant list taken by SelectField containing all the characters
+char_choices = [('Fox', 'Fox'), ('Falco', 'Falco'), ('Sheik', 'Sheik'), ('Marth', 'Marth'), ('Jigglypuff', 'Jigglypuff'), ('Peach', 'Peach'), ('Captain Falcon', 'Captain Falcon'), ('Ice Climbers', 'Ice Climbers'), ('Dr. Mario', 'Dr. Mario'), ('Pikachu', 'Pikachu'), ('Samus', 'Samus'), ('Ganondorf', 'Ganondorf'), ('Luigi', 'Luigi'), ('Mario', 'Mario'), ('Young Link', 'Young Link'), ('Link', 'Link'), ('Donkey Kong', 'Donkey Kong'), ('Yoshi', 'Yoshi'), ('Zelda', 'Zelda'), ('Roy', 'Roy'), ('Mewtwo', 'Mewtwo'), ('Mr. Game and Watch', 'Mr. Game and Watch'), ('Ness', 'Ness'), ('Bowser', 'Bowser'), ('Pichu', 'Pichu'), ('Kirby', 'Kirby')]
+
+
 class UserCreate(Form):
   user_tag = StringField('tag', validators=[DataRequired()])
   user_region = StringField('region', validators=[DataRequired()])
-  user_main = StringField('main', validators=[DataRequired()])
+  user_main = SelectField('main', choices=char_choices, coerce=str, validators=[DataRequired()])
 
 class SetCreate(Form):
   set_winner_tag = StringField('winner_tag', validators=[DataRequired()])
@@ -15,14 +19,13 @@ class SetCreate(Form):
   set_max_match_count = SelectField('Best of:', choices = [('1','1'), ('3','3'), ('5','5'), ('7','7')], validators=[Required()]) # choices are strings, not integers, but are converted to integers in views.py - this is because it was buggy otherwise.
 
 class MatchSubmit(Form):
-  char_list = [('Fox', 'Fox'), ('Falco', 'Falco'), ('Sheik', 'Sheik'), ('Marth', 'Marth'), ('Jigglypuff', 'Jigglypuff'), ('Peach', 'Peach'), ('Captain Falcon', 'Captain Falcon'), ('Ice Climbers', 'Ice Climbers'), ('Dr. Mario', 'Dr. Mario'), ('Pikachu', 'Pikachu'), ('Samus', 'Samus'), ('Ganondorf', 'Ganondorf'), ('Luigi', 'Luigi'), ('Mario', 'Mario'), ('Young Link', 'Young Link'), ('Link', 'Link'), ('Donkey Kong', 'Donkey Kong'), ('Yoshi', 'Yoshi'), ('Zelda', 'Zelda'), ('Roy', 'Roy'), ('Mewtwo', 'Mewtwo'), ('Mr. Game and Watch', 'Mr. Game and Watch'), ('Ness', 'Ness'), ('Bowser', 'Bowser'), ('Pichu', 'Pichu'), ('Kirby', 'Kirby')] # Choices list for winner_char and loser_char SelectField; a constant value containing all the characters and not an input
-  
-  match_stage = SelectField('match_stage', choices = [('Battlefield', 'Battlefield'), ('Dream Land', 'Dream Land'), ('Final Destination', 'Final Destination'), ('Fountain of Dreams', 'Fountain of Dreams'), ('Pokemon Stadium', 'Pokemon Stadium'), ('Yoshi\'s Story', 'Yoshi\'s Story'), ('Other', 'Other')], coerce=str, validators=[Required()]) # optional input, so doesn't check if left blank
-  
+  match_stage = SelectField('match_stage', choices = [('Battlefield', 'Battlefield'), ('Dream Land', 'Dream Land'), ('Final Destination', 'Final Destination'), ('Fountain of Dreams', 'Fountain of Dreams'), ('Pokemon Stadium', 'Pokemon Stadium'), ('Yoshi\'s Story', 'Yoshi\'s Story'), ('Other', 'Other')], coerce=str)
+
+  # Data not required in case no match info is known (no validators for fields)
   match_winner = StringField('match_winner')
   match_loser = StringField('match_loser')
-  winner_char = SelectField('winner_char', choices=char_list) # Data no required in case no match info is known
-  loser_char = SelectField('loser_char', choices=char_list)
+  winner_char = SelectField('winner_char', choices=char_choices)
+  loser_char = SelectField('loser_char', choices=char_choices)
 
 # SelectField format for choices: The first (value, label) is the actual value. The label is what appears in the dropdown menu. In this case, both should be the samei
 
