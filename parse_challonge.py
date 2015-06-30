7959d6c3d9c2d22c7e9d49087fbb6ed6a2d724cc
Lawrence Luo <luolawrence1@gmail.com>
2015-06-30 15:52:16 -0700

    parsing is now done not using substrings but tag ids. Compiled all functioning methods into parse_challonge, and successfully tested importing to database

--- parse_challonge.py
+++ parse_challonge.py
@@ -1,157 +1,195 @@
 #!flask/bin/python
 # Currently supports: fully loaded Challonge Bracket with: seed or no seed displayed, score=int or \xe2\x9c\x93 (checkmark) unicode, data-round=int, <span> title=str
 # Note: Challonge refers to Sets as "match". I will adopt the term "match" to represent Set only in this file, for the sake of simplicity.
+from app.models import *
 
 import urllib3
 from bs4 import BeautifulSoup
 import re
 
 # Given list of top_match_half items, parse lines to get relevant data for round, tag, seed, and score
-def parse_top_match(tag_list):
-  for tag in tag_list:
-    match_round = str(tag.find_all("div", {"class" : "inner_content"}))
-    span_tag = str(tag.find("span")) 
-    div_seed = str(tag.find_all("div", {"class" : "top_seed"}))
-    div_score = str(tag.find_all("div", {"class" : "top_score"}))
+def parse_top_match(top_item_list):
+  # dictionary which will contain relevant data
+  top_half = {}
 
-    # dictionary which will contain relevant data
-    top_half = {}
+  for item in top_item_list:
+    round_item = item.find("div", {"class" : "inner_content"})
+    round_number = round_item['data-round']
+    if round_number:
+      top_half["round"] = int(round_number)
 
-    if match_round and len(match_round) > 2:
-      start_text = "data-round=\""
-      end_text = "\"><"
+    span_item = item.find("span")
+    tag = span_item.getText()
+    if tag:
+      top_half["tag"] = str(tag)
 
-      start_index = match_round.find(start_text) + 12
-      end_index = match_round.find(end_text)
-      top_half["round"] = match_round[start_index:end_index]
-
-    if span_tag and len(span_tag) > 0:
-      processed_span_tag = '~' + span_tag[1:]
-      start_index = processed_span_tag.index('>') + 1
-      end_index = processed_span_tag.index('<')
-      top_half["tag"] = processed_span_tag[start_index:end_index]
-    
-    if div_seed and len(div_seed) > 2:
-      processed_div_seed = '~' + div_seed[2:]
-      start_index = processed_div_seed.index('>') + 1
-      end_index = processed_div_seed.index('<')
-      top_half["seed"] = processed_div_seed[start_index:end_index]
-
-
-    if div_score and len(div_score) > 2:
-      processed_div_score = '~' + div_score[2:] 
-      start_index = processed_div_score.index('>') + 1
-      end_index = processed_div_score.index('<') 
-      # Check for alternate win scores: checkmark Unicode character, empty score
-      if processed_div_score[start_index:end_index] == "\xe2\x9c\x93":
-        top_half["score"] = 1
-      elif processed_div_score[start_index:end_index] == '':
+    seed_item = item.find("div", {"class" : "top_seed"})
+    seed_number = seed_item.getText()
+    if seed_number:
+      top_half["seed"] = int(seed_number)
+      
+    score_item = item.find("div", {"class" : "top_score"})
+    score = score_item.string
+    if score:
+      if score == u"\u2713":
+        top_half["score"] = 1 
+      elif score == "":
         top_half["score"] = 0
       else:
-        top_half["score"] = 0
+        top_half["score"] = int(score)
+    else:
+      top_half["score"] = 0
 
     return top_half
 
-# given list of bottom_half items, parse lines to get relevant data for round, tag, seed, and score
-def parse_bottom_match(tag_list):
-  for tag in tag_list:
-    match_round = str(tag.find_all("div", {"class" : "inner_content"}))
-    span_tag = str(tag.find("span")) 
-    div_seed = str(tag.find_all("div", {"class" : "bottom_seed"}))
-    div_score = str(tag.find_all("div", {"class" : "bottom_score"}))
+# Given list of top_match_half items, parse lines to get relevant data for round, tag, seed, and score
+def parse_bottom_match(bottom_item_list):
+  # dictionary which will contain relevant data
+  bottom_half = {}
 
-    # dictionary containing relevant data to be returned
-    bottom_half  = {}
-    
-    if match_round and len(match_round) > 2:
-      start_text = "data-round=\""
-      end_text = "\"><"
+  for item in bottom_item_list:
+    round_item = item.find("div", {"class" : "inner_content"})
+    round_number = round_item['data-round']
+    if round_number:
+      bottom_half["round"] = int(round_number)
 
-      start_index = match_round.find(start_text) + 12
-      end_index = match_round.find(end_text)
-      bottom_half["round"] = match_round[start_index:end_index]
+    span_item = item.find("span")
+    tag = span_item.getText()
+    if tag:
+      bottom_half["tag"] = str(tag)
 
-    if span_tag and len(span_tag) > 0:
-      processed_span_tag = '~' + span_tag[1:]
-      start_index = processed_span_tag.index('>') + 1
-      end_index = processed_span_tag.index('<')
-      bottom_half["tag"] = span_tag[start_index:end_index]
-    
-    if div_seed and len(div_seed) > 2:
-      processed_div_seed = '~' + div_seed[2:]
-      start_index = processed_div_seed.index('>') + 1
-      end_index = processed_div_seed.index('<')
-      bottom_half["seed"] = processed_div_seed[start_index:end_index]
-
-    if div_score and len(div_score) > 2:
-      processed_div_score = '~' + div_score[2:] 
-      start_index = processed_div_score.index('>') + 1
-      end_index = processed_div_score.index('<') 
-      # Check for alternate win scores: checkmark Unicode character, empty score
-      if processed_div_score[start_index:end_index] == "\xe2\x9c\x93":
-        bottom_half["score"] = 1
-      elif processed_div_score[start_index:end_index] == '':
+    seed_item = item.find("div", {"class" : "bottom_seed"})
+    seed_number = seed_item.getText()
+    if seed_number:
+      bottom_half["seed"] = int(seed_number)
+      
+    score_item = item.find("div", {"class" : "bottom_score"})
+    score = score_item.string
+    if score:
+      if score == u"\u2713":
+        bottom_half["score"] = 1 
+      elif score == "":
         bottom_half["score"] = 0
       else:
-        bottom_half["score"] = 0
+        bottom_half["score"] = int(score)
+    else:
+      bottom_half["score"] = 0
 
     return bottom_half
 
-# currently grabbing from: CEO 2014 Top 32 Bracket
-Tournament = "http://ceogaming.challonge.com/ceo2014ssbmtop32"
-conn = urllib3.connection_from_url(Tournament)
-r1 = conn.request("GET", Tournament)
-soup = BeautifulSoup(r1.data)
-soup.prettify()
+def parse_challonge(tournament_url):
+  conn = urllib3.connection_from_url(tournament_url)
+  r1 = conn.request("GET", tournament_url)
+  soup = BeautifulSoup(r1.data)
+  soup.prettify()
 
-# find all matches
-# first line identifying a match: <td class='core' id='match_qtip_23317245_' style='padding:0'>
-all_matches = soup.find_all("td", {"class" : "core"}, id=re.compile("\S"))
-for match in all_matches:
-  print str(match)
+  # find all matches
+  # first line identifying a match: <td class='core' id='match_qtip_23317245_' style='padding:0'>
+  all_matches = soup.find_all("td", {"class" : "core"}, id=re.compile("\S"))
+
+  # a dictionary that stores match information by round number. Each dictionary key represents a round number in winners or losers side of the bracket. As per Challonge, -1 = losers round 1, while 1 = winners round 1
+  matches_by_round = {}
+  # a list of lists of pairs of dictionaries. The dictionary pair represents a match, with top_half and bottom_half data stored in the inner list index 0 and 1 respectively. The outer list contains each match representation, iterable by index.
+  matchlist = []
+
+  for div_tag in all_matches:
+    # create inner list, to store the two dictionaries that represent a match
+    current_match = []
+
+    match_top = div_tag.find_all("div", {"class" : "match_top_half"})
+    # pass list containing all match_top_half to parse_top_match, which will parse lines separately for data
+    top_half = parse_top_match(match_top)
+    # append dictionary from parse_top_match into the inner list. This is one dictionary representing the top_half
+    current_match.append(top_half)
+    print top_half
+
+    # add match half to dictionary by round; if no key exists for round, create one
+    if top_half['round'] is not None:    
+      round_num = matches_by_round.setdefault(top_half['round'], [])
+      round_num.append(top_half)
+
+    match_bottom = div_tag.find_all("div", {"class" : "match_bottom_half"})
+    # pass list containing all match_bottom_half to parse_bottom_match, which will parse lines separately for data
+    bottom_half = parse_bottom_match(match_bottom)
+    # append dictionary from bottom_half into the same inner list as top_half. This is one dictionary representing the bottom_half
+    current_match.append(bottom_half)
+    print bottom_half
+
+    # add match half to dictionary by round; if no key exists for round, create one
+    if bottom_half['round'] is not None:    
+      round_num = matches_by_round.setdefault(top_half['round'], [])
+      round_num.append(bottom_half)
+    
+    print current_match
+    # append the inner list representing this match to the outer list, which will eventually contain all matches in the bracket.
+    matchlist.append(current_match)
+    print '\n'
+
+  print matches_by_round
   print '\n'
-print "done printing all_matches"
-print '\n'
+  for match in matchlist:
+    print match
 
-# a dictionary that stores match information by round number. Each dictionary key represents a round number in winners or losers side of the bracket. As per Challonge, -1 = losers round 1, while 1 = winners round 1
-matches_by_round = {}
-# a list of lists of pairs of dictionaries. The dictionary pair represents a match, with top_half and bottom_half data stored in the inner list index 0 and 1 respectively. The outer list contains each match representation, iterable by index.
-matchlist = []
+  return matchlist
 
-for div_tag in all_matches:
-  # create inner list, to store the two dictionaries that represent a match
-  current_match = []
 
-  match_top = div_tag.find_all("div", {"class" : "match_top_half"})
-  # pass list containing all match_top_half to parse_top_match, which will parse lines separately for data
-  top_half = parse_top_match(match_top)
-  # append dictionary from parse_top_match into the inner list. This is one dictionary representing the top_half
-  current_match.append(top_half)
-  print top_half
+# Get: score, tag, seed, round given a matchlist from parse_challonge, and a string tournamnent name (to fill out Set.attribute)
+def import_challonge_data(matchlist, tournament_name):
+  for set in matchlist:
+    top_player = set[0]
+    bottom_player = set[1]
 
-  # add match half to dictionary by round; if no key exists for round, create one
-  if top_half['round'] != None:    
-    round_num = matches_by_round.setdefault(top_half['round'], [])
-    round_num.append(top_half)
+    # Determine the set winner and loser based on their scores. Once assigned, no more references 
+    # will be made to the ambiguous top and bottom player, but instead to set_winner and set_loser
+    # Both set_winner and set_loser are DICTIONARIES. Scores are stored in respective variables
+    if top_player['score'] > bottom_player['score']:
+      set_winner = top_player
+      set_loser = bottom_player
+      winner_score = top_player['score']
+      loser_score = bottom_player['score']
+    else:
+      set_winner = bottom_player
+      set_loser = top_player
+      winner_score = bottom_player['score']
+      loser_score = top_player['score']
 
-  match_bottom = div_tag.find_all("div", {"class" : "match_bottom_half"})
-  # pass list containing all match_bottom_half to parse_bottom_match, which will parse lines separately for data
-  bottom_half = parse_bottom_match(match_bottom)
-  # append dictionary from bottom_half into the same inner list as top_half. This is one dictionary representing the bottom_half
-  current_match.append(bottom_half)
-  print bottom_half
+    # After calling this function, Users by 'tag' will exist in database in any case.
+    # stores User object in respective variables
+    winner_user = check_set_user(set_winner['tag'])
+    loser_user = check_set_user(set_loser['tag'])
 
-  # add match half to dictionary by round; if no key exists for round, create one
-  if bottom_half['round'] != None:    
-    round_num = matches_by_round.setdefault(top_half['round'], [])
-    round_num.append(bottom_half)
-  
-  print current_match
-  # append the inner list representing this match to the outer list, which will eventually contain all matches in the bracket.
-  matchlist.append(current_match)
-  print '\n'
+    print winner_user
+    print loser_user
 
-print matches_by_round
-print '\n'
-for match in matchlist:
-  print match
+    # If seed doesn't exist, do nothing; else, assign to User attribute seed
+    if set_winner['seed']:
+      winner_user.seed = set_winner['seed']
+    if set_loser['seed']:
+      loser_user.seed = set_loser['seed']
+
+    # Get round number; if they match, store the round variable; if they don't (some error occurred), ignore it.
+    if int(set_winner['round']) == int(set_loser['round']):
+      round_number = int(set_winner['round'])
+    else:
+      # prevent crashing when creating the Set without existing variable round_number
+      round_number = 0
+
+    # Convert scores to integers
+
+    new_set = Set(tournament=tournament_name,
+                  round_type=round_number,
+                  winner_tag=winner_user.tag,
+                  loser_tag=loser_user.tag,
+                  winner_id=winner_user.id,
+                  loser_id=loser_user.id,
+                  winner_score=winner_score,
+                  loser_score=loser_score,
+                  total_matches=winner_score+loser_score)
+
+    db.session.add(new_set)
+    print new_set
+    print '\n'
+
+  db.session.commit()
+
+  return "Import Successful"
--- parse_challonge_unit_test.py
+++ parse_challonge_unit_test.py
@@ -1,91 +1,17 @@
 #!flask/bin/python
-#Currently using fake setlist item
 
-import os
-import unittest
-
-from config import basedir
 from app import app, db
 from app.models import *
+
+import urllib3
+from bs4 import BeautifulSoup
+import re
+
 from parse_challonge import *
 
-setlist = [[{'score': '1', 'tag': 'P4K EMP Armada', 'seed': '1', 'round': '1'}, {'score': '0', 'tag': 'GK Pi', 'seed': '2', 'round': '1'}], 
-[{'score': '1', 'tag': 'CTRL DJ Nintendo', 'seed': '3', 'round': '1'}, {'score': '0', 'tag': 'GameGuys Mojo', 'seed': '4', 'round': '1'}], 
-[{'score': '1', 'tag': 'Fiction', 'seed': '5', 'round': '1'}, {'score': '0', 'tag': 'Jake13', 'seed': '6', 'round': '1'}], 
-[{'score': '1', 'tag': 'Crs.Chillindude', 'seed': '7', 'round': '1'}, {'score': '0', 'tag': 'GK Gahtzu', 'seed': '8', 'round': '1'}], 
-[{'score': '1', 'tag': 'Crs.Hungrybox', 'seed': '9', 'round': '1'}, {'score': '0', 'tag': 'Zidane', 'seed': '10', 'round': '1'}], 
-[{'score': '1', 'tag': 'Liquid`KDJ', 'seed': '11', 'round': '1'}, {'score': '0', 'tag': 'CTRL The Moon', 'seed': '12', 'round': '1'}]]
-
-print "Testing selections from setlist"
-print "setlist[1] should be DJ Nintendo vs Mojo"
-print setlist[1]
-print '\n'
-print "setlist[1][0] should be DJ Nintendo's top half entry"
-print setlist[1][0]
-print '\n'
-print "setlist[1][0]['tag'] should be the tag CTRL DJ Nintendo"
-print setlist[1][0]['tag']
-print '\n'
-
-Tournament = "CEO 2014"
-
-# Get: score, tag, seed, round
-for set in setlist:
-	top_player = set[0]
-	bottom_player = set[1]
-
-	# Determine the set winner and loser based on their scores. Once assigned, no more references 
-	# will be made to the ambiguous top and bottom player, but instead to set_winner and set_loser
-	# Both set_winner and set_loser are DICTIONARIES. Scores are stored in respective variables
-	if top_player['score'] > bottom_player['score']:
-		set_winner = top_player
-		set_loser = bottom_player
-		winner_score = top_player['score']
-		loser_score = bottom_player['score']
-	else:
-		set_winner = bottom_player
-		set_loser = top_player
-		winner_score = bottom_player['score']
-		loser_score = top_player['score']
-
-	# After calling this function, Users by 'tag' will exist in database in any case.
-	# stores User object in respective variables
-	winner_user = check_set_user(set_winner['tag'])
-	loser_user = check_set_user(set_loser['tag'])
-
-	print winner_user
-	print loser_user
-
-	# If seed doesn't exist, do nothing; else, assign to User attribute seed
-	if set_winner['seed']:
-		winner_user.seed = set_winner['seed']
-	if set_loser['seed']:
-		loser_user.seed = set_loser['seed']
-
-	# Get round number; if they match, store the round variable; if they don't (some error occurred), ignore it.
-	if int(set_winner['round']) == int(set_loser['round']):
-		round_number = int(set_winner['round'])
-	else:
-		# prevent crashing when creating the Set without existing variable round_number
-		round_number = 0
-
-	# Convert scores to integers
-
-	new_set = Set(tournament=Tournament,
-								round_type=round_number,
-								winner_tag=winner_user.tag,
-								loser_tag=loser_user.tag,
-								winner_id=winner_user.id,
-								loser_id=loser_user.id,
-								winner_score=winner_score,
-								loser_score=loser_score,
-								total_matches=winner_score+loser_score)
-
-	db.session.add(new_set)
-	print new_set
-	print '\n'
-
-db.session.commit()
+# Simulation of what would happen if one called these methods from parse_challonge from anywhere
+matchlist = parse_challonge("http://ceogaming.challonge.com/ceo2014ssbmtop32")
+import_challonge_data(matchlist, "CEO 2014")
 
 print "Printing Userlist and Setlist"
 userlist = User.query.all()
