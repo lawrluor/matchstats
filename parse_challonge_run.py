157f2c9fd759f3fa36fc176ceaba27e580fb5f8d
Lawrence Luo <luolawrence1@gmail.com>
2015-08-26 11:01:29 -0700

    modularized parsing process in parse_tournament_list, assigned to parse_challonge_run

--- parse_challonge_run.py
+++ parse_challonge_run.py
@@ -13,16 +13,53 @@ from parse_challonge_standings import *
 from parse_challonge_info import *
 from parse_challonge_matches import *
 
-def main():
-  tournament_url = sys.argv[1]
-  tournament_name = sys.argv[2]  
-  tournament_region = sys.argv[3]
-  tournament_date = sys.argv[4]
+# Helper function for read_tournamentlist
+# Parses relevant info regarding a tournament given string containing tournament name, url, region, and date
+# New\ Game\ Plus\ 1|http://bigbluees.challonge.com/NGP1|New\ England|04/28/2015 
+# Returns: dictionary with relevant keywords attaced to values
+def process_tournament_line(tournament_line):
+  # process each line and parse tournament_url, tournament_name 
+  processed_tournament_line = tournament_line.strip('\n')
+  line_parser = re.compile('[|]')
+  tokens = line_parser.split(processed_tournament_line)
+  tournament_name = tokens[0]
+  # Remove '\' escape characters
+  tournament_name = tournament_name.translate(None, '\\')
 
-  # Check for region, if not specific Region, set to None
+  tournament_url = tokens[1]
+  tournament_region = tokens[2]
+  tournament_date = tokens[3]
+  
+  # If region passed as string "None", "Global", "National", convert to None object
   if tournament_region=="None" or tournament_region=="Global" or tournament_region=="National":
     tournament_region = None
-  
+  else:
+    tournament_region = tournament_region.translate(None, '\\')
+
+  info_dict = { 'tournament_name' : tournament_name,
+               'tournament_region' : tournament_region,
+               'tournament_url' : tournament_url,
+               'tournament_date' : tournament_date
+             }
+
+  print info_dict
+  return info_dict
+
+# Takes parameter String tournament_line, a block of tournament info
+# Runs functions to add Tournament to database
+# "New\ Game\ Plus\ 1|http://bigbluees.challonge.com/NGP1|New\ England|04/28/2015"
+# Returns None
+def parse_challonge_run(tournament_line):
+  # Parse the tournament_line, and assign values to local vars
+  tournament_info = process_tournament_line(tournament_line)
+  tournament_name = tournament_info['tournament_name']
+  tournament_url = tournament_info['tournament_url']
+  tournament_region = tournament_info['tournament_region']
+  tournament_date = tournament_info['tournament_date']
+
+  if tournament_region=="None" or tournament_region=="Global" or tournament_region=="National":
+    tournament_region = None
+
   # Convert passed parameter date to datetime object if not None
   if tournament_date!="None":
     tournament_date = convert_int_date(tournament_date)
@@ -43,35 +80,13 @@ def main():
   matchlist = parse_challonge_matches(tournament_url, tournament_region)
   # Create Set objects with matchlist, and append them to the relationship between Tournament and Sets
   import_challonge_matches(matchlist, tournament_name, tournament_region, False)
+
+def main():
+  tournament_line = sys.argv[1]
+  parse_challonge_run(tournament_line)
 
 if __name__ == "__main__":
 	main()
-
-# This function is identical to main, but can be called outside of the terminal (namely in parse_challonge_tournamentlist.py, which adds Challonge tournaments to the database given a text file list of Challonge tournaments.
-def parse_challonge_run(tournament_url, tournament_name, tournament_region, tournament_date):
-  if tournament_region=="None" or tournament_region=="Global" or tournament_region=="National":
-    tournament_region = None
-
-  # Convert passed parameter date to datetime object if not None
-  if tournament_date!="None":
-    tournament_date = convert_int_date(tournament_date)
-  else:
-    tournament_date = None
-
-  # Create and return dictionary of values for Tournament attributes
-  tournament_info = parse_challonge_info(tournament_url)
-  # create new Tournament by assigning the tournament_info dictionary, given the tournament name as well.
-  new_tourney = import_challonge_info(tournament_info, tournament_name, tournament_url, tournament_region, tournament_date)
- 
-  # Returns dictionary of placements : tags given tournament url
-  all_placements = parse_challonge_standings(tournament_url, tournament_region)
-  # given the dictionary of placements and the Tournament object, link Tournament and User with Placement object. Users checked and sanitized here.
-  import_challonge_standings(all_placements, new_tourney)
-
-  # given the tournament_url, return a list of dictionaries with values for Set attributes
-  matchlist = parse_challonge_matches(tournament_url, tournament_region)
-  # Create Set objects with matchlist, and append them to the relationship between Tournament and Sets
-  import_challonge_matches(matchlist, tournament_name, tournament_region, False)
 
 def parse_challonge_pool(pool_url, parent_tournament_name, tournament_region, advance_number):
   # Query to find parent tournament (Final Bracket Tournament) by name
@@ -81,3 +96,29 @@ def parse_challonge_pool(pool_url, parent_tournament_name, tournament_region, ad
 
   matchlist = parse_challonge_matches(pool_url, tournament_region)
   import_challonge_matches(matchlist, parent_tournament.name, tournament_region, True)
+
+# This function is obsolete, takes in tournament info in different format. 
+def parse_challonge_run_alt(tournament_url, tournament_name, tournament_region, tournament_date):
+  if tournament_region=="None" or tournament_region=="Global" or tournament_region=="National":
+    tournament_region = None
+
+  # Convert passed parameter date to datetime object if not None
+  if tournament_date!="None":
+    tournament_date = convert_int_date(tournament_date)
+  else:
+    tournament_date = None
+
+  # Create and return dictionary of values for Tournament attributes
+  tournament_info = parse_challonge_info(tournament_url)
+  # create new Tournament by assigning the tournament_info dictionary, given the tournament name as well.
+  new_tourney = import_challonge_info(tournament_info, tournament_name, tournament_url, tournament_region, tournament_date)
+ 
+  # Returns dictionary of placements : tags given tournament url
+  all_placements = parse_challonge_standings(tournament_url, tournament_region)
+  # given the dictionary of placements and the Tournament object, link Tournament and User with Placement object. Users checked and sanitized here.
+  import_challonge_standings(all_placements, new_tourney)
+
+  # given the tournament_url, return a list of dictionaries with values for Set attributes
+  matchlist = parse_challonge_matches(tournament_url, tournament_region)
+  # Create Set objects with matchlist, and append them to the relationship between Tournament and Sets
+  import_challonge_matches(matchlist, tournament_name, tournament_region, False)
--- parse_challonge_tournamentlist.py
+++ parse_challonge_tournamentlist.py
@@ -17,29 +17,11 @@ def read_tournamentlist(challonge_tournamentlist):
   # f is a file object
   with open(challonge_tournamentlist, 'r') as f:
     for tournament_line in f:
-      # process each line and parse tournament_url, tournament_name 
-      processed_tournament_line = tournament_line.strip('\n')
-      line_parser = re.compile('[|]')
-      tokens = line_parser.split(processed_tournament_line)
-      tournament_name = tokens[0]
-      # Remove '\' escape characters
-      tournament_name = tournament_name.translate(None, '\\')
-
-      tournament_url = tokens[1]
-      tournament_region = tokens[2]
-      tournament_date = tokens[3]
-      
-      # If region passed as string "None", "Global", "National", convert to None object
-      if tournament_region=="None" or tournament_region=="Global" or tournament_region=="National":
-        tournament_region = None
-      else:
-        tournament_region = tournament_region.translate(None, '\\')
-
-      print tournament_name + ': ' + tournament_url
-      print tournament_region, tournament_date
-
-      parse_challonge_run(tournament_url, tournament_name, tournament_region, tournament_date)
+      info = process_tournament_line(tournament_line)
+      parse_challonge_run(info['tournament_url'], info['tournament_name'], info['tournament_region'], info['tournament_date'])
   f.close()
 
 if __name__ == "__main__":
   main()
+
+
